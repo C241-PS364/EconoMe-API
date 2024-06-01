aaffe1c15a1242523d737e7b36e2cd5e7b84b2af
Juanda <116890415+ItsCielo@users.noreply.github.com>
2024-06-01 14:10:41 +0700

    Template Awal
    Express.js authentication API using PostgreSQL as the database

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('PostgreSQL Connected...');
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDB };
