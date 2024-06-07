dbf6172b6e091012a85c2e9a4614d4d559a77a35
Chrystalio <chrystalio.k@outlook.com>
2024-06-07 21:56:35 +0700

    Refactor server.js

--- server.js
+++ server.js
@@ -1,24 +1,7 @@
+const app = require('./app');
 
-const express = require('express');
-const pool = require('./config/db');
-const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
-
-// Test Route
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.status(200).json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
-});
-
-// Start Server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
