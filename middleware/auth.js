4dd25a04cd94a9cca1c25badaa7ede27cc305c04
Chrystalio <chrystalio.k@outlook.com>
2024-06-09 05:14:36 +0700

    Add middleware for handling JWT Authentication

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied, token missing!' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid token, ' });
+  }
+};
+
+module.exports = authenticateToken;
