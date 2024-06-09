b841a54035c9c51692d2dcdcbf867f923abfa75b
Chrystalio <chrystalio.k@outlook.com>
2024-06-10 05:39:36 +0700

    Refactor register method, update handle return response format

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,31 +6,30 @@ require('dotenv').config();
 
 const jwtSecret = process.env.JWT_SECRET;
 
-
 const register = async (req, res) => {
   const { username, password, name, date_of_birth, gender, job } = req.body;
 
   if (!username || !password || !name || !date_of_birth || !gender || !job) {
-    return res.status(400).json({ error: 'All fields are required' });
+    return res.status(400).json({ error: true, message: 'All fields are required' });
   }
 
   const formattedDate = moment(date_of_birth, 'YYYY-MM-DD', true);
   if (!formattedDate.isValid()) {
-    return res.status(400).json({ error: 'Invalid date format. Use YYYY-MM-DD' });
+    return res.status(400).json({ error: true, message: 'Invalid date format. Use YYYY-MM-DD' });
   }
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await pool.query(
-      'INSERT INTO users (uuid, name, username, password, date_of_birth, gender, job) VALUES (gen_random_uuid(), $1, $2, $3, $4, $5, $6) RETURNING *',
+    await pool.query(
+      'INSERT INTO users (uuid, name, username, password, date_of_birth, gender, job) VALUES (gen_random_uuid(), $1, $2, $3, $4, $5, $6)',
       [name, username, hashedPassword, formattedDate.format('YYYY-MM-DD'), gender, job]
     );
-    res.status(201).json(result.rows[0]);
+    res.status(201).json({ error: false, message: 'User Created' });
   } catch (error) {
     if (error.code === '23505' && error.constraint === 'users_username_key') {
-      res.status(409).json({ error: 'Username already exists' });
+      res.status(409).json({ error: true, message: 'Username already exists' });
     } else {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: true, message: error.message });
     }
   }
 };
