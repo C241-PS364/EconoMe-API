18d9921ea20ad2692239531b8fe620d394d3047d
Chrystalio <chrystalio.k@outlook.com>
2024-06-09 21:05:46 +0700

    Regist route editProfile

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getProfile } = require('../../controllers/authController');
+const { editProfile } = require('../../controllers/userController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
 router.get('/profile', authenticateToken, getProfile);
+router.put('/profile', authenticateToken, editProfile);
 
 module.exports = router;
