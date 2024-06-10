3c97e1a9f30fcb39e70349f38464f232ba47900f
Chrystalio <chrystalio.k@outlook.com>
2024-06-10 20:55:18 +0700

    Add /refresh-token endpoint to auth routes

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const { register } = require('../../controllers/authController');
-const { login } = require('../../controllers/authController');
-
-
+const { register, login, refresh } = require('../../controllers/authController');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/refresh-token', refresh);
 
 module.exports = router;
