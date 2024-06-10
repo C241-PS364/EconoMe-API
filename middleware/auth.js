84d67e7f2f8ca4eeacff529d450fec907c182028
Chrystalio <chrystalio.k@outlook.com>
2024-06-10 20:54:32 +0700

    Improve error handling in authenticateToken middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,27 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const jwtSecret = process.env.JWT_SECRET;
-
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ error: 'Access denied, token missing!' });
+    return res.status(401).json({ error: true, message: 'Token missing' });
   }
 
-  try {
-    const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded;
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(403).json({ error: true, message: 'Token expired' });
+      } else if (err.name === 'JsonWebTokenError') {
+        return res.status(403).json({ error: true, message: 'Invalid token' });
+      } else {
+        return res.status(403).json({ error: true, message: 'Token verification failed' });
+      }
+    }
+    req.user = user;
     next();
-  } catch (error) {
-    res.status(401).json({ error: 'Invalid token!' });
-  }
+  });
 };
 
 module.exports = authenticateToken;
