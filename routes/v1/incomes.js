777ec3b2a9c632a6cb7ff62daa487f83918c3c9c
chrystalio <chrystalio.k@outlook.com>
2024-06-13 23:41:55 +0700

    Refactor incomes routes standarize the route format

--- routes/v1/incomes.js
+++ routes/v1/incomes.js
@@ -1,16 +1,12 @@
 const express = require('express');
+const { getIncomeById, createIncome, updateIncome, deleteIncome } = require('../../controllers/incomesController');
+const authenticateToken = require('../../middleware/auth');
+
 const router = express.Router();
 
-const {
-    getIncomeById,
-    createIncome,
-    updateIncome,
-    deleteIncome
-} = require('../../controllers/incomesController');
-
-router.get('/:id', getIncomeById);
-router.post('/input', createIncome);
-router.put('/:id', updateIncome);
-router.delete('/:id', deleteIncome);
+router.get('/:id', authenticateToken, getIncomeById);
+router.post('/', authenticateToken, createIncome);
+router.put('/:id', authenticateToken, updateIncome);
+router.delete('/:id', authenticateToken, deleteIncome);
 
 module.exports = router;
