3503f90bd756d27cedf08cb638c92d267ff88d21
Chrystalio <chrystalio.k@outlook.com>
2024-06-14 21:52:49 +0700

    Add route for fetching all expenses

--- routes/v1/expense.js
+++ routes/v1/expense.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createExpense, updateExpense, deleteExpense } = require('../../controllers/expenseController');
+const { getExpenses, createExpense, updateExpense, deleteExpense } = require('../../controllers/expenseController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
+router.get('/', authenticateToken, getExpenses);
 router.post('/', authenticateToken, createExpense);
 router.put('/:id', authenticateToken, updateExpense);
 router.delete('/:id', authenticateToken, deleteExpense);
