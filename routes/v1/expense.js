cdca8b219579344f76d9946c1f6ddb928f12b3d6
Chrystalio <chrystalio.k@outlook.com>
2024-06-14 21:31:22 +0700

    Add route for deleting expenses

--- routes/v1/expense.js
+++ routes/v1/expense.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { createExpense, updateExpense } = require('../../controllers/expenseController');
+const { createExpense, updateExpense, deleteExpense } = require('../../controllers/expenseController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
 router.post('/', authenticateToken, createExpense);
 router.put('/:id', authenticateToken, updateExpense);
+router.delete('/:id', authenticateToken, deleteExpense);
 
 
 module.exports = router;
