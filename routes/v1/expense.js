3549b26dfb797e1896601c2556208870c86cdcd4
Chrystalio <chrystalio.k@outlook.com>
2024-06-14 21:23:37 +0700

    Add route for updating expenses

--- routes/v1/expense.js
+++ routes/v1/expense.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createExpense } = require('../../controllers/expenseController');
+const { createExpense, updateExpense } = require('../../controllers/expenseController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
 router.post('/', authenticateToken, createExpense);
+router.put('/:id', authenticateToken, updateExpense);
+
 
 module.exports = router;
