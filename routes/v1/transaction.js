7aae9e683d2d44d1587184808032a28137f896ef
Chrystalio <chrystalio.k@outlook.com>
2024-06-16 20:55:10 +0700

    Remove getTopSpending method and routes

--- routes/v1/transaction.js
+++ routes/v1/transaction.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { getTransactions, getTopSpending } = require('../../controllers/transactionController');
+const { getTransactions } = require('../../controllers/transactionController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getTransactions);
-router.get('/top-spending', authenticateToken, getTopSpending);
 
 module.exports = router;
