fe51974fc4f73f5aeb4ac9239c46534616b85d6b
chrystalio <chrystalio.k@outlook.com>
2024-06-20 20:27:55 +0700

    Remove unused comments

--- routes/v1/predict.js
+++ routes/v1/predict.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const { predictExpense } = require('../../controllers/monthlyPredictionController');
 const { predictTimeSeries } = require('../../controllers/timeseriesPredictionController');
-
-// const { anotherPrediction } = require('../../controllers/anotherPredictionController');
 const authenticateToken = require('../../middleware/auth');
 
 const router = express.Router();
 
 router.get('/monthly-expense', authenticateToken, predictExpense);
 router.get('/timeseries-expense', authenticateToken, predictTimeSeries);
-
-// router.get('/another-prediction', authenticateToken, anotherPrediction);
 
 module.exports = router;
