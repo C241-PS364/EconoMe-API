d9569856c7b0632a8f250e38a95a4e5921460b9f
chrystalio <chrystalio.k@outlook.com>
2024-06-20 22:48:36 +0700

    Extend loadModel.js to include budget category model URLs

--- utils/loadModel.js
+++ utils/loadModel.js
@@ -3,6 +3,12 @@ const tf = require('@tensorflow/tfjs-node');
 const modelUrls = {
   monthlyExpensePrediction: 'https://storage.googleapis.com/econome-models/monthly-expense-prediction/model.json',
   timeSeriesPrediction: 'https://storage.googleapis.com/econome-models/time-series-prediction/model.json',
+  housing: 'https://storage.googleapis.com/econome-models/budgeting-category/housing/model.json',
+  food: 'https://storage.googleapis.com/econome-models/budgeting-category/food/model.json',
+  transportation: 'https://storage.googleapis.com/econome-models/budgeting-category/transportation/model.json',
+  entertainment: 'https://storage.googleapis.com/econome-models/budgeting-category/entertainment/model.json',
+  personal_care: 'https://storage.googleapis.com/econome-models/budgeting-category/personal_care/model.json',
+  miscellaneous: 'https://storage.googleapis.com/econome-models/budgeting-category/miscellaneous/model.json',
 };
 
 const loadModel = async (modelName) => {
